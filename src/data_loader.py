c75b75b6283c68a1dce92a7a81c21f847f1d7713
rafael albuquerque <rafa.albuquerque.oliveira@gmail.com>
2024-11-01 12:49:20 -0300

    Added modules

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,21 +1,12 @@
 import pandas as pd
 
 class DataLoader:
-    """Class to load data from CSV files."""
-
-    def __init__(self, file_path):
-        """
-        Initializes the DataLoader with the specified file path.
-
-        Args:
-            file_path (str): Path to the CSV file containing the dataset.
-        """
-        self.file_path = file_path
-
+    def __init__(self, data_file):
+        self.data_file = data_file
 
     def load_data(self):
-        """Loads data from the specified CSV file and returns features and labels."""
-        df = pd.read_csv(self.file_path)
+        """Load dataset from CSV file."""
+        df = pd.read_csv(self.data_file)
         X = df['comment_text']
-        y = df[df.columns[2:]].values  # Adjust based on your label columns
+        y = df[df.columns[2:]].values
         return X, y
