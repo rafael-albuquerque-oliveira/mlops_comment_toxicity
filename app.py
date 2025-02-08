470da312a42e214fd195de217912da9a0d35227b
rafael albuquerque <rafa.albuquerque.oliveira@gmail.com>
2025-02-08 11:46:33 -0300

    uodate app url

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.title("Toxic Comment Detector")
 
 user_input = st.text_area("Enter a comment:")
 if st.button("Analyze"):
-    response = requests.post("http://your-api-url/predict", json={"text": user_input})
+    response = requests.post("https://toxic-comments-api-678895434688.us-central1.run.app/predict", json={"text": user_input})
     prediction = response.json()
     st.write(prediction)
